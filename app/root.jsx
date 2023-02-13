292ee531f1bc15ca7b3f8716e4bf216f997bfe28
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-02-13 18:16:18 +0300

    links componeent add for styles

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,18 @@
 import { LiveReload } from "@remix-run/react";
 import { Outlet } from "@remix-run/react";
-import "./styles/index.css";
+import "../app/styles/index.css";
+
+import { Links } from "@remix-run/react";
+import stylesUrl from "./styles/index.css";
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
 
 export default function App() {
   return (
@@ -8,6 +20,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         Hello world
